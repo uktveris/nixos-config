a160b54b5288bec7ef58791f37b19f62080944c6
uktveris <marius.uktveris@gmail.com>
2026-08-07 15:19:12 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # use "nixos", or some hostname as the name of the configuration
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
